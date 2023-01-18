7d0ffd95b57e0bec12f243516cb6c05ba82e048c
meghanakolapalli <maggi.kolapalli@gmail.com>
2023-01-18 09:19:36 +0530

    files

---
+++ src/main/java/gradle1/cal.java
@@ -0,0 +1,18 @@
+package gradle1;
+
+public class cal {
+	public float calculateInterset(float p,float r,float t) {
+		return(p*t*r/100);
+	}
+
+	public static void main(String[] args) {
+		cal cal1=new cal();
+		System.out.println(cal1.calculateInterset(4000,25,2));
+		// TODO Auto-generated method stub
+
+	}
+		// TODO Auto-generated method stub
+
+	}
+
+
